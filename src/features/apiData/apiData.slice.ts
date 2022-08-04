5a8150a4cb5244eacffcc68a52397a249a766df3
Jose Osorio <joseinvictus273@hotmail.com>
2022-08-03 19:44:27 -0500

    Ready to deploy

--- src/features/apiData/apiData.slice.ts
+++ src/features/apiData/apiData.slice.ts
@@ -1,38 +1,41 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { Movies, Genres, MoviesByGenrer, MovieDetail } from '../../types'
 
+
+const apiKey = import.meta.env.VITE_APP_APIKEY
+
 export const apiData = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({baseUrl: 'https://api.themoviedb.org/3/'}),
     endpoints(builder){
         return {
             fetchTrendMovies: builder.query<Movies, string|void> ({
-                query(apiKey = '216c20e5c1bee70856e7107fd576117f'){
+                query(apiKey = import.meta.env.VITE_APP_APIKEY){
                     return `trending/movie/day?api_key=${apiKey}`
                 }
             }),
             fetchCategories: builder.query<Genres, string|void>({
-                query(apiKey = '216c20e5c1bee70856e7107fd576117f'){
+                query(apiKey = import.meta.env.VITE_APP_APIKEY){
                     return `genre/movie/list?api_key=${apiKey}`
                 }
             }),
             fetchMovieByCategory: builder.query<MoviesByGenrer, string|void>({
-                query(id, apiKey = '216c20e5c1bee70856e7107fd576117f'){
+                query(id, apiKey = import.meta.env.VITE_APP_APIKEY){
                     return `discover/movie?api_key=${apiKey}&with_genres=${id}`
                 }
             }),
             fetchMovieDetail: builder.query<MovieDetail, string|void>({
-                query(id, apiKey = '216c20e5c1bee70856e7107fd576117f'){
+                query(id, apiKey = import.meta.env.VITE_APP_APIKEY){
                     return `movie/${id}?api_key=${apiKey}`
                 }
             }),
             fetchRecommendedMovies: builder.query<Movies, string|void>({
-                query(id, apiKey = '216c20e5c1bee70856e7107fd576117f'){
+                query(id, apiKey = import.meta.env.VITE_APP_APIKEY){
                     return `movie/${id}/recommendations?api_key=${apiKey}`
                 }
             }),
             fetchSearchMovie: builder.query<Movies, string|void>({
-                query(query, apiKey = '216c20e5c1bee70856e7107fd576117f'){
+                query(query, apiKey = import.meta.env.VITE_APP_APIKEY){
                     return `search/movie?api_key=${apiKey}&query=${query}`
                 }
             })
